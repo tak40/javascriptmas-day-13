805968f0b1b9c6891393011a19029b223155f1da
Tak <takeshiseodev@gmail.com>
2023-12-13 19:22:12 -0500

    update: final

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 /**
-Task:
-- Write the code to help a user choose the perfect Christmas dinner idea
-  based on the number of people attending.
-- Include a checkbox for the user to specify the meal 
-  should be vegetarian-friendly.
-
-Dinner suggestions (or choose your own!):
-Vegetarian: Winter Squash Risotto
-4 people or less: Ham
-5+ people: Turkey
-
-Stretch goals:
-- Add more dietary options.
-- Show recipes when the meal has been selected.
+ * Task:
+ * - Write the code to help a user choose the perfect Christmas dinner idea
+ *   based on the number of people attending.
+ * - Include a checkbox for the user to specify the meal
+ *   should be vegetarian-friendly.
+ *
+ * Dinner suggestions (or choose your own!):
+ * Vegetarian: Winter Squash Risotto
+ * 4 people or less: Ham
+ * 5+ people: Turkey
+ *
+ * Stretch goals:
+ * - Add more dietary options.
+ * - Show recipes when the meal has been selected.
+ *
+ * @format
  */
 
+// Cache DOM elements
+const btn = document.getElementById('btn')
+const numInput = document.getElementById('num-input')
+const vegetarianInput = document.getElementById('vegetarian-input')
+const foodDisplay = document.getElementById('food')
+
+btn.addEventListener('click', () => {
+    const numGuests = +numInput.value // Unary plus for number conversion
+    const isVegetarian = vegetarianInput.checked
+
+    if (isNaN(numGuests) || numGuests <= 0) {
+        alert('Please enter a valid number of guests.')
+        return
+    }
+
+    // Ternary operator for conditional assignment
+    const dinnerSuggestion = isVegetarian
+        ? 'Winter Squash Risotto'
+        : numGuests <= 4
+        ? 'Ham'
+        : 'Turkey'
+
+    foodDisplay.textContent = dinnerSuggestion
+})
